f0e604864f029eee6b9c7bfb88e3a7c0df61af10
karankumar56 <kk3942022@gmail.com>
2024-05-19 16:29:04 +0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 
-public class App {
+public class App{
     public static void main(String[] args) throws Exception {
         int boardWidth = 600;
         int boardHeight = boardWidth;
